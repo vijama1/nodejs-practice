072b49f60e88bf43f37a5a9752d4ec6c1b175c4f
Aman Vijay <amanvijay.cse19@jecrc.ac.in>
2019-01-02 14:56:35 +0530

    non blocking codes

---
+++ nonblockingcodes.js
@@ -0,0 +1,6 @@
+var fs=require("fs");
+var data=fs.readFile("input.txt",function(err,data){
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
+console.log("Program Ended");
