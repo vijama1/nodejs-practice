cbc72301d6cfb1fbfd3823f48785b387b736c32f
vijama1 <amanvijay.cs@gmail.com>
2019-01-08 18:00:30 +0530

    generating random whole number

--- randomwholenumber.js
+++ randomwholenumber.js
@@ -1,8 +1,18 @@
-var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+// Example
+function ourRandomRange(ourMin, ourMax) {
 
-function randomWholeNum() {
-
-  // Only change code below this line.
-
-  return Math.floor(Math.random()*10);
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
 }
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random()*(myMax-myMin+1))+myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
