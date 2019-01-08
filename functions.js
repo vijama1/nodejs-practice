64ab7cd7851b15ee857721be1b5436f24cee1bca
vijama1 <amanvijay.cs@gmail.com>
2019-01-08 13:05:32 +0530

    functions in javascript

---
+++ functions.js
@@ -0,0 +1,13 @@
+// Example
+/*function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+*/
+// Only change code below this line.
+
+function functionwithArgs(arg1,arg2){
+  console.log(arg1+arg2);
+}
+functionwithArgs(1,2);
+functionwithArgs(7,9);
