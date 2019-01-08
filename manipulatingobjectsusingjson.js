b914ac3ac499d1eac6828a2beffb017dfec6675c
vijama1 <amanvijay.cs@gmail.com>
2019-01-08 17:18:32 +0530

    manipulating objects using json

---
+++ manipulatingobjectsusingjson.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist":"Chainsmokers",
+    "title":"sick boy",
+    "release_year":2018,
+    "formats":[
+      "CD",
+      "iTunes",
+      "LT"
+    ],
+    "gold":true
+  }
+];
