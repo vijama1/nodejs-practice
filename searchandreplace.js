7099b0c4097fb3f79c8ee5a9013f4eaf76859558
vijama1 <amanvijay.cs@gmail.com>
2019-01-09 17:35:37 +0530

    search and replace

---
+++ searchandreplace.js
@@ -0,0 +1,5 @@
+let huhText = "This sandwich is good.";
+let fixRegex = /good/; // Change this line
+let replaceText = "okey-dokey"; // Change this line
+let result = huhText.replace(fixRegex, replaceText);
+console.log(result)
